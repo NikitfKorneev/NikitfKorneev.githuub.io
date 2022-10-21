79ae16ad475de59e32a2f5a58af39223c85f6509
NikitfKorneev <109066903+NikitfKorneev@users.noreply.github.com>
2022-10-21 15:20:15 +0300

    Create data.php

---
+++ data.php
@@ -0,0 +1,33 @@
+<?php
+  $fields = [
+    'last_name' => [
+      'Фамилия',
+      'required' => 1,
+    ],
+    'first_name' => ['Имя',
+    'required' => 1,
+  ],
+    'second_name' => ['Отчество',
+    'required' => 1,
+  ],
+    'email' => ['Email',
+    'required' => 1,
+  ],
+    'text' => ['text',
+    'required' => 1,
+  ],
+  'text' => ['text',
+  'required' => 1,
+],
+'radio-test' => ['radio-test',
+  'required' => 1,
+],
+'radio-test' => ['radio-test',
+  'required' => 1,
+],
+'file1' => ['file1',
+  'required' => 1,
+],
+  ]
+  
+ ?>
